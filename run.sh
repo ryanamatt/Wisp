35d94b53161983263a24bef25a38a6eddf1492df
Ryan Mattson <ryanmattson@outlook.com>
2026-08-25 09:51:18 -0500

    Add Logo

--- run.sh
+++ run.sh
@@ -2,5 +2,9 @@
 
 set -euo pipefail
 
-echo ./build/wisp -f ~/Projects/wisp/qml -m ~/Projects/wisp/build/qml run
-./build/wisp -f ~/Projects/wisp/qml -m ~/Projects/wisp/build/qml run
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+export WISP_SHARE_DIR="${SCRIPT_DIR}"
+
+echo ./build/wisp -f "${SCRIPT_DIR}/qml" -m "${SCRIPT_DIR}/build/qml" run
+./build/wisp -f "${SCRIPT_DIR}/qml" -m "${SCRIPT_DIR}/build/qml" run
