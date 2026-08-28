eba7ca87e57e18b9d60e07d873af1d77ae06479d
Ryan Mattson <ryanmattson@outlook.com>
2026-08-28 15:15:10 -0500

    Add Window Screenshot flag to script

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -5,21 +5,23 @@ mkdir -p "$SAVE_DIR"
 
 SAVE_FILE=false
 FULL_MONITOR=false
+WINDOW=false
 FILENAME=""
 
 function usage() {
-  echo "Usage: $0 [-s] [-w] [-h|--help]"
+  echo "Usage: $0 [-s] [-m] [-w] [-h|--help]"
   echo "  -s             Save the screenshot to $SAVE_DIR"
-  echo "  -w             Takes a Screenshot of Entire Current Montior"
+  echo "  -m             Takes a screenshot of the entire current monitor"
+  echo "  -w             Takes a screenshot of a specific window"
   echo "  -h, --help     Show this help message"
   exit 0
 }
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    -s)
-      SAVE_FILE=true;;
-    -w) FULL_MONITOR=true ;;
+    -s) SAVE_FILE=true ;;
+    -m) FULL_MONITOR=true ;;
+    -w) WINDOW=true ;;
     -h|--help) usage ;;
     *) echo "Unknown option: $1"; usage ;;
   esac
@@ -31,6 +33,9 @@ TMP_FILE=$(mktemp)
 if [ "$FULL_MONITOR" = true ]; then
   GEOMETRY=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | "\(.x),\(.y) \(.width)x\(.height)"')
   grim -g "$GEOMETRY" - > "$TMP_FILE"
+elif [ "$WINDOW" = true ]; then
+  GEOMETRY=$(hyprctl activewindow -j | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
+  grim -g "$GEOMETRY" - > "$TMP_FILE"
 else
   grim -g "$(slurp)" - > "$TMP_FILE"
 fi
@@ -39,7 +44,6 @@ wl-copy < "$TMP_FILE"
 
 if [ "$SAVE_FILE" = true ]; then
   FILENAME="$(date +%Y-%m-%d_%H-%M-%S).png"
-  
   mv "$TMP_FILE" "$SAVE_DIR/$FILENAME"
   echo "Saved to $SAVE_DIR/$FILENAME"
   notify-send "Screenshot Saved" "File saved to $SAVE_DIR/$FILENAME"
